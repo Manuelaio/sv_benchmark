6b18a6f8501cb9113441b28c9fef2b7d67d7e332
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2021-06-08 10:20:27 +0200

    Update annotate_svaba_sv_nw.R

--- SVABA/annotate_svaba_sv_nw.R
+++ SVABA/annotate_svaba_sv_nw.R
@@ -11,29 +11,41 @@ tmp_vcf<-tmp_vcf[-(grep("#CHROM",tmp_vcf)+1):-(length(tmp_vcf))]
 cols <- colnames(read.table(pipe(paste0('grep -v "##" ',vcf ,' | grep "#"| sed s/#//')), header = TRUE))
 cols <- sapply(cols, function(x) gsub("(mrkdp\\.)|(\\.bam)", "", x))
 svaba_uniq1 <- read.table(vcf, stringsAsFactors = FALSE)
-#elimino columns in svaba not necessary
+#remove not necessary columns 
                
 svaba_uniq= svaba_uniq1[-c(10,11,12)]
 colnames(svaba_uniq)= cols
 rm(svaba_uniq1)
 
-
-# ann_sv_svaba.vcf
 file=read.table(bed_ann, stringsAsFactors = F)
 colnames(file)[2]='POS'
+###rearrange vcf 
+
+info_field=data.frame(do.call('rbind', strsplit(as.character(file$V8),';',fixed=TRUE)))
+file_df= cbind(file, info_field)
+file_df$X2= gsub("SVLEN=", "", file_df$X2)
+file_df$X4= gsub("GT=", "", file_df$X4)
+file_df$X2=as.numeric(file_df$X2)
+
+file_df$END= file_df$POS + file_df$X2
+bed= file_df[c(1:2,13,3,12)]
+
+write.table(bed, file="NA12878.rearrange.bed", 
+            quote = F, row.names = F, col.names = T, sep="\t")
+
+# ann_sv_svaba.vcf
+
+
 library(dplyr)
 
 df=inner_join(file, svaba_uniq, by='POS')
 
 a= df[!duplicated(df[1:7]),]
 df1= a[c(9:17,3)]
-#df.f= df1[!duplicated(df1),]
+
 colnames(df1)[10]="sv_type"
 colnames(df1)[1]="#CHROM"
 df1$INFO<- paste(df1$INFO,df1$sv_type, sep=";SVTYPE=")
-#df.f$END= df.f$ALT[gsub(".*:","", df.f$ALT),]
-
-
 
 write.table(df1, file="/work/emanuela.iovino/intersect_SV/new_analysis/svaba/NA12878/svaba_for_surv.vcf", 
             quote = F, row.names = F, col.names = T, sep="\t")
